c059a3d505abb9a23baf269ee4006e69afaaf512
jisanglee <newspatoy1@gmail.com>
2021-05-26 15:10:14 +0900

    make portfolio page

--- src/Pages/PortfoliosPage.js
+++ src/Pages/PortfoliosPage.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../Components/Button';
+import Menu from '../Components/Menu';
+import Title from '../Components/Title';
+import portfolios from '../data/portfolios';
+import { InnerLayout, MainLayout } from '../styles/Layouts';
+
+const allButtons = ['All',...new Set(portfolios.map(item=>item.category))]
 
 function PortfoliosPage(props) {
+    const [menuItem, setMenuItems] = useState(portfolios);
+    const [button, setButtons] = useState(allButtons);
+
+    const filter = (button) => {
+        if (button === 'All') {
+            setMenuItems(portfolios)
+            return;
+        }
+        const filteredData = portfolios.filter(item => item.category === button);
+        setMenuItems(filteredData);
+    }
     return (
-        <div>
-            
-        </div>
+        <MainLayout>
+            <Title title={'Portfolios'} span={'Portfolios'} />
+            <InnerLayout>
+                <Button filter={filter} button={button} />
+                <Menu menuItems={menuItem} />
+            </InnerLayout>
+        </MainLayout>
     );
 }
 
---
+++ src/data/portfolios.js
@@ -0,0 +1,46 @@
+import img1 from '../img/portImages/maya-1.jpg';
+import img2 from '../img/portImages/maya-4.jpg';
+import img3 from '../img/portImages/maya-3.jpg';
+
+
+const portfolios = [
+    {
+        id: 1,
+        category: 'Python',
+        image: img1,
+        link1: 'https://www.google.com',
+        link2: 'https://www.google.com',
+        title: 'How To use Python',
+        text:'Number one Programming language',
+    },
+    {
+        id: 2,
+        category: 'Javascript',
+        image: img2,
+        link1: 'https://www.google.com',
+        link2: 'https://www.google.com',
+        title: 'Javascript Refactoring',
+        text:'How refactoring my Javascript Code?',
+    },
+    {
+        id: 3,
+        category: 'R',
+        image: img3,
+        link1: 'https://www.google.com',
+        link2: 'https://www.google.com',
+        title: 'R Data Analysis',
+        text:'Using R data snalysis',
+    },
+    {
+        id: 4,
+        category: 'R',
+        image: img3,
+        link1: 'https://www.google.com',
+        link2: 'https://www.google.com',
+        title: 'R Data Analysis',
+        text:'Using R data snalysis',
+    },
+]
+
+
+export default portfolios;
